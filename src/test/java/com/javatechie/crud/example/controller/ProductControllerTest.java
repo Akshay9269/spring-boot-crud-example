533c03663fdaeb7245836a52c20d4316e6810f2f
Akshay9269 <84924723+Akshay9269@users.noreply.github.com>
2025-02-23 10:47:18 +0530

    Adding test file of controller

---
+++ src/test/java/com/javatechie/crud/example/controller/ProductControllerTest.java
@@ -0,0 +1,16 @@
+package com.javatechie.crud.example.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ProductControllerTest {
+
+    @Test
+    void testAddProduct_WhenProvidedWithValidDetails_ReturnsExpectedResult(){
+
+    }
+
+}
